a0133a24b6f9c40a50fc03641ee3ddc1183be8df
KingScroll <69558037+KingScroll@users.noreply.github.com>
2024-06-30 18:25:31 -0400

    Change create asset menu categorization

--- Assets/SO Architecture/Utility/SOArchitecture_Utility.cs
+++ Assets/SO Architecture/Utility/SOArchitecture_Utility.cs
@@ -2,13 +2,17 @@
 {
     public static class SOArchitecture_Utility
     {
+        // Create Asset Menu Variables 
+
+        public const string ASSETROOT = "SO Architecture/";
+
         public const int ASSET_MENU_ORDER_VARIABLES = 121;
         public const int ASSET_MENU_ORDER_EVENTS = 122;
         public const int ASSET_MENU_ORDER_COLLECTIONS = 123;
 
-        public const string VARIABLE_SUBMENU = "Variables/";
-        public const string COLLECTION_SUBMENU = "Collections/";
-        public const string GAME_EVENT = "Game Events/";
+        public const string VARIABLE_SUBMENU = ASSETROOT + "Variables/";
+        public const string COLLECTION_SUBMENU = ASSETROOT + "Collections/";
+        public const string GAME_EVENT = ASSETROOT + "Game Events/";
 
         public const string ADVANCED_GAME_EVENT = GAME_EVENT + "Advanced/";
         public const string ADVANCED_VARIABLE_SUBMENU = VARIABLE_SUBMENU + "Advanced/";
